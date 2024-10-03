92a503ff5c6fcef0f614511894978409c9b9ae42
Rohan Patil <146317921+rohan7003@users.noreply.github.com>
2024-10-03 12:54:07 +0000

    done

--- server.js
+++ server.js
@@ -1,17 +1,77 @@
-const http = require("http");
-const port = 8081;
-const toDoList = ["Hey everyone","hope u all","are doing awesome","at your work"];
-http.createServer((req, res)=>{
-   const{method, url} = req;
-   // console.log(method, url);
+const http = require("http"); //importing http
 
-   if(url === "/todos"){
-        console.log("todos route")
-   }else if (url === "/"){
-        console.log("/home default route")
-   }
-   res.end();
+const port = 8081; //port num
+
+const toDoList = ["hey everyone", "hope u all", "are doing", "awesome at ur work"]; // array
+
+// creating a server
+http.createServer((req, res)=>{
+    // call back funs
+    const {method, url} = req;
+    // console.log(method, url);
+
+    if(url === "/todos"){
+        if(method === "GET"){
+        console.log("todos route, n its a GET Method")
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(toDoList.toString())
+    }else if(method === "POST"){
+        let body = "";
+        req.on('error', (err)=>{
+            console.log(err)
+        }).on('data', (chunk)=>{
+            body += chunk;
+            console.log("chunk: ", chunk);
+
+           
+        }).on('end', ()=>{
+            body = JSON.parse(body);
+            console.log("data: ", body)
+            let newToDo = toDoList;
+            newToDo.push(body.item);
+        })
+
+    }else if(method === "PUT"){
+
+    }else if(method === "DELETE"){
+        let body = "";
+        req.on('error',(err)=>{
+            console.log(err)
+        }).on('data', (chunk)=>{
+            body += chunk;
+        }).on('end',()=>{
+            body = JSON.parse(body);
+            let deleteThis = body.item;
+
+            // for(let i=0; i<toDoList.length; i++){
+            //     if(toDoList[i]===deleteThis){
+            //         toDoList.splice(i,1);
+            //         break;
+            //     }
+            // }
+
+            toDoList.find((elem, index)=>{
+                if(elem === deleteThis){
+                    toDoList.splice(index,1)
+                }
+            })
+            
+        })
+    }
+    
+    else{
+        res.writeHead(501);
+    }
+    }else if(url === "/"){
+        console.log("/ home default route")
+    }
+
+    res.end();
 })
 .listen(port,()=>{
     console.log(`NodeJs Server Started Running On Port ${port}`);
 })
+
+
+// http://localhost:8081/
+
